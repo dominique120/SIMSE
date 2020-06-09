3cc66adae479ff421dca3539ff2c07761c0dede6
Dominique <dominique120@users.noreply.github.com>
2020-06-09 17:50:40 -0500

    Cambio

--- GUI/PERSONA/UTIL/Direcciones/frmNewDireccion.Designer.cs
+++ GUI/PERSONA/UTIL/Direcciones/frmNewDireccion.Designer.cs
@@ -214,7 +214,7 @@
             this.btnGuardar.Name = "btnGuardar";
             this.btnGuardar.Size = new System.Drawing.Size(75, 23);
             this.btnGuardar.TabIndex = 3;
-            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.Text = "Guardar1";
             this.btnGuardar.UseVisualStyleBackColor = true;
             this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
             // 
