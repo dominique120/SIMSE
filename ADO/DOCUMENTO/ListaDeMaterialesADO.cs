21088a05a0bf01ead03d12c2cb225663e4eaf6f5
Dominique <dominique120@users.noreply.github.com>
2020-07-07 19:43:04 -0500

    Capa Datos Lista

---
+++ ADO/DOCUMENTO/ListaDeMaterialesADO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.DOCUMENTO {
+    class ListaDeMaterialesADO {
+        Conection conection = new Conection();
+        SqlConnection con = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+
+        public DataTable ListarMaterialesPorProyecto(int id_proyecto) {
+            DataSet dts = new DataSet();
+            try {
+                con.ConnectionString = conection.GetCon();
+                cmd.Connection = con;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "DOCUMENTO.ListarMaterialesPorProyecto";
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@id_proyecto", id_proyecto);
+
+                SqlDataAdapter adapter;
+                adapter = new SqlDataAdapter(cmd);
+                adapter.Fill(dts, "ListaMateriales");
+                return dts.Tables["ListaMateriales"];
+            } catch (SqlException ex) {
+                throw new Exception("Error mostrando las Listas de Materiales: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ ADO/DOCUMENTO/ReporteSupervisionADO.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.DOCUMENTO {
+    public class ReporteSupervisionADO {
+        Conection conection = new Conection();
+        SqlConnection con = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+
+        
+        public DataTable ListarReporteSupervisionFull() {
+            DataSet dts = new DataSet();
+            try {
+                con.ConnectionString = conection.GetCon();
+                cmd.Connection = con;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "DOCUMENTO.ListarReporteSupervisionFull";
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                adapter.Fill(dts, "ReporteSupervision");
+            } catch (Exception ex) {
+                throw new Exception("Error mostrando Reportes de Supervision: " + ex.Message);
+            }
+            return dts.Tables["ReporteSupervision"];
+        }
+
+        public DataTable ListarReporteSupervisionProyectoSupervisor(int id_proyecto, int id_supervisor) {
+            DataSet dts = new DataSet();
+            try {
+                con.ConnectionString = conection.GetCon();
+                cmd.Connection = con;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "DOCUMENTO.ListarReporteSupervisionPorProyectoEmpleado";
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@id_proyecto", id_proyecto);
+                cmd.Parameters.AddWithValue("@id_supervisor", id_supervisor);
+
+                SqlDataAdapter adapter;
+                adapter = new SqlDataAdapter(cmd);
+                adapter.Fill(dts, "ReporteSupervision");
+                return dts.Tables["ReporteSupervision"];
+            } catch (SqlException ex) {
+                throw new Exception("Error mostrando Reportes de Supervision: " + ex.Message);
+            }
+        }
+    }
+}
